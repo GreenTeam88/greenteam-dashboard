42a4303cca8979e3f6c77d34b7b7ec649f3d9fcb
elmirshidev <rzazadeelmir19@gmail.com>
2024-06-10 19:12:00 +0300

    new update on next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
   prettier: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = withSvgr(nextConfig);
