ea77e0bf759417d7ed4028f04d3950700d19bf26
yahya <elmokhtariyahya2001@gmail.com>
2024-10-27 02:12:55 +0100

    style:  missing logo in login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,13 +2,16 @@
 
 import React from 'react';
 
+import { ReactComponent as LogoWhite } from '@/assets/icons/LogoWhite.svg';
 import LoginForm from '@/components/auth/LoginForm';
 import withPublicAccess from '@/hoc/withPublicAccess';
 
 function LoginPage() {
   return (
     <div className={'flex h-full'}>
-      <div className={'bg-bgPrimaryGreen flex-1 flex justify-center items-center'}></div>
+      <div className={'bg-bgPrimaryGreen flex-1 flex justify-center items-center'}>
+        <LogoWhite />
+      </div>
       <div className={'bg-white flex-[2] flex justify-center items-center'}>
         <LoginForm />
       </div>
